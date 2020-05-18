5db4f8231bca4019a88893eefb71b7f1a4710790
karolinakondziolka <karolina.kondziolka@gmail.com>
2020-05-18 20:35:55 +0200

    7.1 Poem Beautifier v2

--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -3,8 +3,7 @@ package com.kodilla.stream.beautifier;
 public class PoemBeautifier {
 
     public void beautify(String text, PoemDecorator poemDecorator){
-        String result= poemDecorator.decorate(text);
+        String result = poemDecorator.decorate(text);
         System.out.println(result);
-
     }
 }
